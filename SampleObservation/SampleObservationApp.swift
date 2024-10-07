8e20ef6b6ac395718e6f8609bec72e11c0494749
Hardik Modha <hardik.modha27@gmail.com>
2024-10-07 19:25:44 +0530

    Initial Commit

---
+++ SampleObservation/SampleObservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleObservationApp.swift
+//  SampleObservation
+//
+//  Created by Hardik Modha on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleObservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
